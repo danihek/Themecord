1f122668604c5c0b4cdd2a5f8c9d34d72d3c9412
danihek <danihek07@gmail.com>
2024-08-09 16:26:14 +0200

    colors script update, readme update

--- README.md
+++ README.md
@@ -43,12 +43,12 @@ and your discord client will update it's theme.
 ## Installation (Manual)
 1. Install supported discord client
 2. Copy ```colors-discord.css``` from ```./css``` folder to your ```~/.config/wal/templates/```
-3. Copy ```Themecord.css``` from ```./css``` folder to your ```~/.config/DISCORDCLIENT/themes/```
+3. Copy ```Themecord.css``` and ```ThemecordFiller.css.themecord``` from ```./css``` folder to your ```~/.config/DISCORDCLIENT/themes/```
 4. In your discord client, go to themes and select Themecord from available themes.
 5. Now when will you change your wallpaper using pywal, run ```./scripts/discordColors.sh``` and your discord theme will be applied.
 
 ## Example usage
-In folder ```./example``` you see example script which you can use as your template in your own script/config and edit to your needs. Enjoy!
+In folder ```./example``` you see example script which you can use as your template in your own script/config and edit to your needs. Enjoy theme'in!!
 
 # Showcase
 ![alt text](https://github.com/danihek/Themecord/blob/main/assets/2024-01-14-022114_hyprshot.png)
--- scripts/discordColors.sh
+++ scripts/discordColors.sh
@@ -1,27 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # RUN THIS SCRIPT RIGHT AFTER PYWAL TO PROPERLY APPLY COLORS!
 
 confPath="/home/$USER/.config"
 
-clientType="" # Vencord, Vesktop
-VENCORD="Vencord"
-VESKTOP="Vesktop"
+clientType=""
+supportedClients=("Vencord" "vesktop")
 
-if [[ "$confPath/$VENCORD" == *"cannot"* ]]; then 
-    if [[ "$confPath/$VESKTOP" == *"cannot"* ]]; then
-        echo "No compatible clients detected!"
-        echo "In order to use this script you have to use compatible discord clients! Check out github page."
-        echo "Exitting..."
-        exit
+function print_client_err() {
+    echo "No compatible clients detected!"
+echo "In order to use this script you have to use compatible discord clients! Check out github page for more info https://github.com/danihek/Themecord"
+    echo "Exitting..."
+}
+
+function print_dc_err() {
+    echo "Cannot find discord-colors.css in wal cache folder."
+    echo "Exitting..."
+}
+
+for client in "${supportedClients[@]}"; do
+    dc_path=~/.config/$client
+    if test -d $(realpath "$dc_path"); then
+        clientType="$client"
+        echo "[SUCCESS] $client DETECTED (path: $dc_path)"
+        break
     else
-        clientType=$VESKTOP
+        echo "[FAILURE] $client not DETECTED (path: $dc_path)"
+        continue
     fi
-else
-    clientType=$VENCORD
+done
+
+if [ -z "$clientType" ]; then
+    print_client_err
+    exit
 fi
 
 walColorsPath="/home/$USER/.cache/wal/colors-discord.css"
+
+if [ ! -f "$walColorsPath" ]; then
+    print_dc_err
+    exit
+fi
+
 themecordPath="/home/$USER/.config/$clientType/themes/Themecord.css"
 
 echo :root { > $themecordPath
