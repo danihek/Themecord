4ef88a39dc75fb21f93fc11b691117e974ca9bd4
danihek <danihek07@gmail.com>
2024-10-09 01:06:31 +0200

    wrong default.nix

--- default.nix
+++ default.nix
@@ -3,20 +3,12 @@
 let
   themecordFiller = (builtins.readFile ./css/ThemecordFiller.css.themecord);
   discordColorsContent = (builtins.readFile ./scripts/themecord.sh);
-  cssPywalFile  = pkgs.writeTextFile {
-    name = "colors-discord.css";
-    destination = "${builtins.getEnv "HOME"}/.config/wal/templates/colors-discord.css";
-    text = (builtins.readFile ./css/colors-discord.css);
-  };
 in
   pkgs.writeScriptBin "themecord" ''
     #!/usr/bin/env bash
-
     themecordFiller=$(cat << 'EOF'
     ${themecordFiller}
     EOF
     )
-
-    ${discordColorsContent}
-  ''
-
+    # ${discordColorsContent}
+    ''
