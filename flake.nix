c3467a4285457c0b6308c7d1896d280fc0fc2ca4
danihek <danihek07@gmail.com>
2024-11-01 01:46:47 +0100

    final update

--- flake.nix
+++ flake.nix
@@ -3,76 +3,18 @@
 
   inputs =
     {
-      nixpkgs.url = "github:nixos/nixpkgs/";
-      home-manager.url = "github:nix-community/home-manager/";
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in
   {
-    packages.${system}.themecord = (import ./default.nix {
+    packages.${system}.default = (import ./default.nix {
       inherit pkgs;
-      extraPackages = [
-        pkgs.wallust
-        pkgs.pywal
-      ];
     });
-
-    homeConfigurations = {
-      username = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        configuration = { config, pkgs, lib, ... }: {
-
-          home.file.".config/wal/templates/colors-discord.css" = ''
-              --color0: {color0};
-              --color1: {color1};
-              --color2: {color2};
-              --color3: {color3};
-              --color4: {color4};
-              --color5: {color5};
-              --color6: {color6};
-              --color7: {color7};
-              --color8: {color8};
-              --color9: {color9};
-              --color10: {color10};
-              --color11: {color11};
-              --color12: {color12};
-              --color13: {color13};
-              --color14: {color14};
-              --color15: {color15};
-              '';
-
-          home.file.".config/wallust/wallust.toml".text = ''
-            [templates]
-            themecord.template = "colors-discord.css"
-            themecord.target = '~/.cache/wallust/colors-discord.css'
-          '';
-
-          home.file.".config/wallust/templates/colors-discord.css".text = ''
-            --color0:  {{ color0 }};
-            --color1:  {{ color1 }};
-            --color2:  {{ color2 }};
-            --color3:  {{ color3 }};
-            --color4:  {{ color4 }};
-            --color5:  {{ color5 }};
-            --color6:  {{ color6 }};
-            --color7:  {{ color7 }};
-            --color8:  {{ color8 }};
-            --color9:  {{ color9 }};
-            --color10: {{ color10 }};
-            --color11: {{ color11 }};
-            --color12: {{ color12 }};
-            --color13: {{ color13 }};
-            --color14: {{ color14 }};
-            --color15: {{ color15 }};
-          '';
-        };
-      };
-    };
   };
 }
 
---
+++ nix/wallust.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, lib, ... }: 
+
+{
+  home.packages = [ pkgs.wallust ];
+
+  home.file.".config/wallust/wallust.toml".text = ''
+      [templates]
+      themecord.template = "colors-discord.css"
+      themecord.target = '~/.cache/wallust/colors-discord.css'
+  '';
+
+  home.file.".config/wallust/templates/colors-discord.css" .text = ''
+    --color0:  {{ color0 }};
+    --color1:  {{ color1 }};
+    --color2:  {{ color2 }};
+    --color3:  {{ color3 }};
+    --color4:  {{ color4 }};
+    --color5:  {{ color5 }};
+    --color6:  {{ color6 }};
+    --color7:  {{ color7 }};
+    --color8:  {{ color8 }};
+    --color9:  {{ color9 }};
+    --color10: {{ color10 }};
+    --color11: {{ color11 }};
+    --color12: {{ color12 }};
+    --color13: {{ color13 }};
+    --color14: {{ color14 }};
+    --color15: {{ color15 }};
+  '';
+}
